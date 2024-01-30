a7c6c452d3da54b1c25fa6356ef7de07d8c81f87
Gabriel <gabrielkleinribas@gmail.com>
2024-01-29 22:08:35 -0300

    Updated bookDAO CRUD methods

--- booktracker/dao/bookDAO.js
+++ booktracker/dao/bookDAO.js
@@ -1,58 +1,71 @@
-const database = require('../database');
+import { query } from "../database.js";
 
-showBooksDAO = async () => {
-    return database.query("SELECT * FROM books ORDER BY id;");
-}
+export class BookDAO {
+  showBooksDAO = async () => {
+    return query("SELECT * FROM books;");
+  };
 
-createBookDAO = async (book) => {
+  createBookDAO = async (book) => {
     const { id, name, author } = book;
 
-    await database.query({
-        text: "INSERT INTO books VALUES ($1, $2, $3);",
-        values: [id, name, author]
-    })
-}
-
-updateBookDAO = async (id, column, newValue) => {
-    const updateQuery = `UPDATE books SET ${column} = $1 WHERE id = $2;`
-
-    await database.query({
-        text: updateQuery,
-        values: [newValue, id]
-    })
-}
-
-deleteBookDAO = async (id) => {
-    await database.query({
-        text: "DELETE FROM books WHERE id = $1;",
-        values: [id]
-    })
-}
-
-getBookById = async (id) => {
-    return database.query({
-        text: "SELECT * FROM books WHERE id = $1",
-        values: [id]
+    return query({
+      text: "INSERT INTO books VALUES ($1, $2, $3);",
+      values: [id, name, author],
     });
-}
+  };
 
-getLastAddedBook = async () => {
-    return database.query("SELECT * FROM books WHERE id = (SELECT MAX(id) FROM books);")
-}
+  updateBookDAO = async (bookToUpdate, id, column, newValue, isUsingCLI) => {
+    if (isUsingCLI) {
+      const updateQuery = `UPDATE books SET ${column} = $1 WHERE id = $2;`;
 
-getStatusDAO = async () => {
-    const count = await database.query("SELECT count(*)::int AS totalBooks, count(DISTINCT author)::int AS totalAuthors FROM books;");
+      return await database.query({
+        text: updateQuery,
+        values: [newValue, id],
+      });
+    }
+
+    const { bookId, name, author } = bookToUpdate;
+
+    return await query({
+      text: "UPDATE books SET name = $1, author = $2 WHERE id = $3;",
+      values: [name, author, bookId],
+    });
+  };
+
+  deleteBookDAO = async (id) => {
+    return query({
+      text: "DELETE FROM books WHERE id = $1;",
+      values: [id],
+    });
+  };
+
+  getBookById = async (id) => {
+    return database.query({
+      text: "SELECT * FROM books WHERE id = $1",
+      values: [id],
+    });
+  };
+
+  getLastAddedBook = async () => {
+    return database.query(
+      "SELECT * FROM books WHERE id = (SELECT MAX(id) FROM books);",
+    );
+  };
+
+  getStatusDAO = async () => {
+    const count = await database.query(
+      "SELECT count(*)::int AS totalBooks, count(DISTINCT author)::int AS totalAuthors FROM books;",
+    );
     const mostReadedAuthors = await database.query(
-        `
-            WITH author_books_count AS (SELECT DISTINCT author, COUNT(1) AS count FROM books GROUP BY 1)
-        
-            SELECT author
-            FROM author_books_count
-            WHERE count = (SELECT MAX(count) FROM author_books_count);
-        `
+      `
+                WITH author_books_count AS (SELECT DISTINCT author, COUNT(1) AS count FROM books GROUP BY 1)
+
+                SELECT author
+                FROM author_books_count
+                WHERE count = (SELECT MAX(count) FROM author_books_count);
+            `,
     );
 
     return { count: count.rows[0], mostReadedAuthors: mostReadedAuthors.rows };
+  };
 }
-
-module.exports = { showBooksDAO, createBookDAO, updateBookDAO, deleteBookDAO, getBookById, getLastAddedBook, getStatusDAO };
