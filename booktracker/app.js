ba5afe9b2d91924214a3a40d470bc6689f0e9ac2
Gabriel <gabrielkleinribas@gmail.com>
2024-01-30 09:19:32 -0300

    Adjusted CLI entry file `app.js` to use imports

--- booktracker/app.js
+++ booktracker/app.js
@@ -1,61 +1,59 @@
-const bookController = require('./controllers/bookController');
-const keypress = require('keypress');
+import { bookController } from "./controllers/bookController.js";
+import keypress from "keypress";
 
-const operations = ['create', 'read (show all)', 'update', 'delete', 'status'];
+const operations = ["create", "read (show all)", "update", "delete", "status"];
 keypress(process.stdin);
 process.stdin.setRawMode(true);
-process.stdin.on('keypress', handleKeyPress);
+process.stdin.on("keypress", handleKeyPress);
+let selectedItem = 0;
 
 activate();
 
 function activate() {
-    process.stdin.resume();
-    selectedItem = 0;
+  process.stdin.resume();
+  selectedItem = 0;
 
-    displayOptions();
+  displayOptions();
 }
 
 function displayOptions() {
-    console.clear();
-    console.log('\n=== BookTracker v1.0 ===\n')
+  console.clear();
+  console.log("\n=== BookTracker v1.0 ===\n");
 
-    console.log('Select the operation: ')
+  console.log("Select the operation: ");
 
-    operations.forEach((option, index) => {
-        index === selectedItem
-            ? console.log(`> ${option}`)
-            : console.log(option);
-    });
+  operations.forEach((option, index) => {
+    index === selectedItem ? console.log(`> ${option}`) : console.log(option);
+  });
 
-    console.log('\n')
+  console.log("\n");
 }
 
 function handleKeyPress(ch, key) {
-    if (key.name === 'down' && selectedItem < operations.length - 1) {
-        selectedItem++;
-    } else if (key.name === 'up' && selectedItem > 0) {
-        selectedItem--;
-    }
-     else if (key.name === 'return') {
-        process.stdin.pause();
-        return handleOperationChoice(selectedItem);
-    }
+  if (key.name === "down" && selectedItem < operations.length - 1) {
+    selectedItem++;
+  } else if (key.name === "up" && selectedItem > 0) {
+    selectedItem--;
+  } else if (key.name === "return") {
+    process.stdin.pause();
+    return handleOperationChoice(selectedItem);
+  }
 
-    displayOptions();
+  displayOptions();
 }
 
 function handleOperationChoice(selectedItem) {
-    const ops = [
-        {'create': 'createBook'},
-        {'read': 'showBooks'},
-        {'update': 'updateBook'},
-        {'delete': 'deleteBook'},
-        {'status': 'getStatus'},
-    ];
-    
-    const validOp = Object.values(ops[selectedItem])[0]
+  const ops = [
+    { create: "createBook" },
+    { read: "showBooks" },
+    { update: "updateBook" },
+    { delete: "deleteBook" },
+    { status: "getStatus" },
+  ];
 
-    validOp
-        ? bookController[validOp]({ activate })
-        : console.log('[ERROR]: Operation not valid');
+  const validOp = Object.values(ops[selectedItem])[0];
+
+  validOp
+    ? bookController[validOp]({ activate })
+    : console.log("[ERROR]: Operation not valid");
 }
