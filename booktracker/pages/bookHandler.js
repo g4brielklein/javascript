b620b3865cc7d266b24590eaee2aceff3dbfd418
Gabriel <gabrielkleinribas@gmail.com>
2024-02-01 09:08:30 -0300

    Finished `create-book` page and created `show-books`

--- booktracker/pages/bookHandler.js
+++ booktracker/pages/bookHandler.js
@@ -1,10 +1,14 @@
+const APIUrl = "http://localhost:3333/books";
+const toastScreenTime = 3000
+
 const bookName = document.getElementById("book-name");
 const bookAuthor = document.getElementById("book-author");
+const resultMessage = document.getElementsByClassName("result-message")[0]
+const showBooksContainer = document.querySelector('.show-books-container');
+const bookCounter = document.querySelector('#book-counter');
 
 function postBook(book) {
   const { name, author } = book;
-
-  const url = "http://localhost:3333/books";
 
   const newBook = {
     name,
@@ -12,9 +16,9 @@ function postBook(book) {
   };
 
   axios
-    .post(url, newBook)
+    .post(APIUrl, newBook)
     .then(successCreateBook)
-    .catch((error) => console.log(error));
+    .catch(errorCreateBook);
 }
 
 const createBook = () => {
@@ -27,17 +31,55 @@ const createBook = () => {
 };
 
 function successCreateBook() {
-  const resultMessageDiv = document.getElementsByClassName("result-message")[0]
-  const bookNameDiv = document.getElementById("book-name");
-  const bookAuthorDiv = document.getElementById("book-author");
+  bookName.value = "";
+  bookAuthor.value = "";
 
-  bookNameDiv.value = "";
-  bookAuthorDiv.value = "";
-
-  resultMessageDiv.classList.add('active')
-  resultMessageDiv.innerHTML = `<p>Book created successfully!</p>`;
+  resultMessage.classList.add('success')
+  resultMessage.innerHTML = `<p>Book created successfully!</p>`;
 
   setInterval(() => {
-    resultMessageDiv.classList.remove('active')
-  }, 3000)
+    resultMessage.innerHTML = `<p></p>`;
+    resultMessage.classList.remove('success')
+  }, toastScreenTime)
+}
+
+function errorCreateBook(error) {
+  console.error(error.response.data.error)
+
+  resultMessage.classList.add('error')
+  resultMessage.innerHTML = `<p>Error on creating book, try again</p>`;
+
+  setTimeout(() => {
+    resultMessage.innerHTML = `<p></p>`;
+    resultMessage.classList.remove('error')
+  }, toastScreenTime)
+}
+
+const getBooks = async () => {
+  const books = await axios.get(APIUrl);
+
+  bookCounter.innerText = `(${books.data.length})`;
+
+  books.data.forEach((book) => {
+    let div = document.createElement('div')
+    div.classList.add('book-item')
+    div.setAttribute('onclick', `handleBookClick('${book.id}')`)
+    div.setAttribute('id', book.id)
+    showBooksContainer.appendChild(div);
+
+    let name = document.createElement('div')
+    name.classList.add('book-name')
+    div.appendChild(name)
+
+    let author = document.createElement('div')
+    author.classList.add('book-author');
+    div.appendChild(author);
+
+    name.innerHTML = `<small>Name:</small> <strong>${book.name}</strong>`;
+    author.innerHTML = `<small>Author:</small> ${book.author}`;
+  })
+}
+
+const handleBookClick = (id) => {
+  console.log(`clicked - id = ${id}`)
 }
