c685f7ce1008095e30431fc9e53e30023662fc34
Gabriel <gabrielkleinribas@gmail.com>
2024-06-08 19:25:09 -0300

    finished ydkjs first challenge

--- you-dont-know-js-book/get-started-comparisons-coercion.js
+++ you-dont-know-js-book/get-started-comparisons-coercion.js
@@ -3,13 +3,20 @@ const dayEnd = "17:45"
 
 function scheduleMeeting(startTime, durationMinutes) {
     const isStartTimeValid = startTime >= dayStart
-    const isFinishTimeValid = (startTime + durationMinutes) <= dayEnd
 
-    console.log(startTime, durationMinutes)
-    console.log(startTime + durationMinutes)
+    const [h, m] = startTime.split(':')
+    const date = new Date()
+    date.setUTCHours(h, m, 0)
+
+    const sum = date.getMinutes() + durationMinutes
+    date.setMinutes(sum)
+    const meetingEndHours = date.getUTCHours() >= 10 ? date.getUTCHours() : `0${date.getUTCHours()}`
+    const meetingEndTime = `${meetingEndHours}:${date.getMinutes()}`
+
+    const isEndTimeValid = meetingEndTime <= dayEnd
 
     // return false if vialoted bounds, true if not
-    return isStartTimeValid && isFinishTimeValid
+    return isStartTimeValid && isEndTimeValid
 }
 
 const wasMeetingScheduled = scheduleMeeting("09:30", 30)
